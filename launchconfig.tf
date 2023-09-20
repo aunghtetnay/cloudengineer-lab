1e559f2bd37b5a49efb06e7b8fb6c1b12d7ccb96
Aung Htet Nay <aunghtetnay.x@gmail.com>
2023-09-20 22:59:03 +0630

    changed image

--- launchconfig.tf
+++ launchconfig.tf
@@ -71,7 +71,7 @@ resource "aws_iam_instance_profile" "session-manager" {
 resource "aws_instance" "bastion" {
   ami                         = lookup(var.amis, var.region)
   instance_type               = "${var.instance_type}"
-  key_name                    =  "terrform"
+  key_name                    =  "protocol-assignment"
   iam_instance_profile        = aws_iam_instance_profile.session-manager.id
   associate_public_ip_address = true
   security_groups            = [aws_security_group.ec2.id]
@@ -87,7 +87,7 @@ resource "aws_launch_configuration" "ec2" {
   image_id                    = lookup(var.amis, var.region)
   instance_type               = "${var.instance_type}"
   security_groups             = [aws_security_group.ec2.id]
-  key_name                    = "terrform"
+  key_name                    = "protocol-assignment"
   iam_instance_profile        = aws_iam_instance_profile.session-manager.id
   associate_public_ip_address = false
   # user_data = <<-EOL
